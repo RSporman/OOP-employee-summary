8b4beec7a3a7a2b8cb4e0dcce48214a95b688c0a
Bobby Sporman <bobby.sporman@yahoo.com>
2020-08-10 14:42:18 -0400

    Updates

--- Develop/test/Intern.test.js
+++ Develop/test/Intern.test.js
@@ -1,3 +1,23 @@
+const Intern = require("../lib/Intern");
+
+test("Can set school via constructor", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
+
 // const Intern = require("../lib/Intern");
 
 // test("Can set school via constructor", () => {
@@ -19,6 +39,6 @@
 // });
 
 
-test("fake", () => {
-  expect(true).toBe(true);
-})
+// test("fake", () => {
+//   expect(true).toBe(true);
+// })
